3ad6977685e273482a9f2f957cf4cfb4ddc98259
ReachableCEO <charles@turnsys.com>
2025-06-30 11:44:39 -0500

    pretty printing now in the framework.

---
+++ includes/pretty_print.sh
@@ -0,0 +1,18 @@
+function print_info()
+{
+  GREEN='\033[0;32m'
+  NC='\033[0m'
+  tput bold
+  echo -e "$GREEN $1${NC}"
+  tput sgr0
+}
+
+function print_error()
+{
+  RED='\033[0;31m'
+  NC='\033[0m'
+  tput bold
+  echo -e "$RED $1${NC}"
+  echo "$1"
+  tput sgr0
+}
