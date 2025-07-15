9de9f44a8bbda203f21b4b1ed030f9de060f9e14
ReachableCEO <charles@turnsys.com>
2025-07-15 18:26:38 -0500

    little more compact.

--- DevEx/UpdateGitVendor.sh
+++ DevEx/UpdateGitVendor.sh
@@ -9,10 +9,8 @@ GIT_VENDOR_LIST="$(git vendor list|grep @|egrep 'main|master')"
 
 IFS=$'\n\t'
 for GIT_VENDOR in ${GIT_VENDOR_LIST[@]}; do
-  export VENDOR_NAME
-  VENDOR_NAME="$(echo $GIT_VENDOR|awk -F '@' '{print $1}')"
-  export VENDOR_REF
-  VENDOR_REF="$(echo $GIT_VENDOR|awk -F '@' '{print $2}'| sed 's/\://g')"
+  export VENDOR_NAME ; VENDOR_NAME="$(echo $GIT_VENDOR|awk -F '@' '{print $1}')"
+  export VENDOR_REF ; VENDOR_REF="$(echo $GIT_VENDOR|awk -F '@' '{print $2}'| sed 's/\://g')"
   git vendor update $VENDOR_NAME $VENDOR_REF
 done
 
