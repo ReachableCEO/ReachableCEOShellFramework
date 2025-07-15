6d339d55d4c8c64226a26dffd5fcb33065a25b52
ReachableCEO <charles@turnsys.com>
2025-07-15 10:52:45 -0500

    Vendored in mo and working on a script to update vendored deps. This framework will be vendored into my other shell projects/microservices. Vendor management automation is critical.

---
+++ DevEx/UpdateGitVendor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#git vendor list|grep @
+#DSR-Pipeline-Server@main:
+#mo@master:
+
+export GIT_VENDOR_LIST
+GIT_VENDOR_LIST="$(git vendor list|grep @|egrep 'main|master')"
+
+IFS=$'\n\t'
+for GIT_VENDOR in ${GIT_VENDOR_LIST[@]}; do
+  export VENDOR_NAME
+  VENDOR_NAME="$(echo $GIT_VENDOR|awk -F '@' '{print $1}')"
+  echo Vendor Name: $VENDOR_NAME
+  export VENDOR_REF
+  VENDOR_REF="$(echo $GIT_VENDOR|awk -F '@' '{print $2}'| sed 's/\://g')"
+  echo Vendor Ref: $VENDOR_REF
+done
+
